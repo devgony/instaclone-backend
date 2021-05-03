a6361a4ec1a84ed631d96c795de2d6852e271db9
devgony <devgony@gmail.com>
2021-05-04 01:01:18 +0900

    searchPhoto with regexp

--- src/photos/searchPhotos/searchPhotos.resolvers.ts
+++ src/photos/searchPhotos/searchPhotos.resolvers.ts
@@ -2,11 +2,16 @@ import client from "../../client";
 
 export default {
   Query: {
-    searchPhotos: (_, { keyword }) =>
+    searchPhotos: async (_, { keyword }) =>
+      // {
+      //   const photos = await client.photo.findMany();
+      //   const regExp = new RegExp(`((?<=\\s)|^)${keyword}`, "gi");
+      //   return photos.filter(photo => photo.caption.match(regExp));
+      // },
       client.photo.findMany({
         where: {
           caption: {
-            startsWith: keyword,
+            contains: keyword,
           },
         },
       }),
